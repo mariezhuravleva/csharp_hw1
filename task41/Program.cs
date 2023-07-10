8b44a6b86c5ff523a2b1a7c99eaf8fc43a41375c
Marie.Zhuravleva <mk1213@bk.ru>
2023-07-10 20:14:47 +0200

    homework 6 (41, 43)

---
+++ task41/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int CountPositiveNumbers(int amountOfNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < amountOfNumbers; i++)
+    {
+        Console.WriteLine($"Enter number {i + 1}:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Enter amount of numbers:");
+int amount = Convert.ToInt32(Console.ReadLine());
+
+int positiveCount = CountPositiveNumbers(amount);
+Console.WriteLine($"Amount of positive numbers: {positiveCount}");
---
+++ task43/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+
+x = (b2 - b1) / (k1 - k2)
+y = k1 * x + b1
+
+k1 = 5, b1 = 2, k2 = 9, b2 = 4 -> (-0,5; -0,5)
+*/
+
+void GetPointOfCrossing(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"The point of crossing is: ({x}; {y}).");
+}
+
+Console.WriteLine("We have two lines described by equations: y = k1 * x + b1, y = k2 * x + b2.");
+
+Console.WriteLine("Enter value of k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value of b1:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value of k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value of b2:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+GetPointOfCrossing(k1, b1, k2, b2);
