49d1c10283f225ea0f0d38ac8a1ca1e081004291
Marie.Zhuravleva <mk1213@bk.ru>
2023-07-14 20:54:50 +0200

    Lesson 7 (46, 48, 49, 51)

---
+++ task49/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+и замените эти элементы на их квадраты.
+
+Например, изначально массив выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть вот так:
+1 4 49 2
+25 9 4 9
+64 4 4 4
+*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeMatrix(int[,] arrayToUpdate)
+{
+    for (int i = 0; i < arrayToUpdate.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToUpdate.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                arrayToUpdate[i,j] *= arrayToUpdate[i,j];
+            }
+        }
+    }
+    return arrayToUpdate;
+}
+
+int[,] randomArray = GetRandom2DArray(3, 4, 10);
+Print2DArray(randomArray);
+Console.WriteLine();
+
+int[,] changedArray = ChangeMatrix(randomArray);
+Print2DArray(changedArray);
---
+++ task51/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumOfDiagonalElements(int[,] array)
+{
+    int sumOfElements = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sumOfElements += array[i,j];
+            }
+        }
+    }
+    return sumOfElements;
+}
+
+int[,] numbers = GetRandom2DArray(5, 4, 10);
+Print2DArray(numbers);
+Console.WriteLine();
+Console.WriteLine($"Sum of diagonal elements: {SumOfDiagonalElements(numbers)}");
